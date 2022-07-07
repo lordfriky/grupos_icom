dabd041edeeed5efbde1f097121d25d08de6ad7e
SistemaRayoXP <edsonarmando78@outlook.com>
2022-07-07 01:47:27 -0500

    Añadido generador dinámico de la web + pseudo-base de datos - Hecho un generador dinámico de la web que toma datos de un JSON y los compila en el index.md - Añadido un JSON con datos de todas las materias de ICOM, ordenadas por clave y NRC y en formato legible por humanos - Mejorado el README.md - Afinaciones en el workflow - Cambios de diseño en el default layout y en el index.md que se genera

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # [Grupos ICOM](https://lordfriky.github.io/grupos_icom)
-Página web para facilitar la búsqueda de grupos de WhatsApp de materias
+Página web para facilitar la búsqueda de grupos de WhatsApp de materias.
 
-# Cómo usarla
-Simplemente entra a la página web, busca la clase en la que estás y entra al grupo de WhatsApp correspondiente. Si aún no ha sido agregado uno y eres el admin de dicho grupo (o tienes el link) puedes agregarlo con el botón ahí mismo, sólo sigue las instrucciones que se muestran en la siguiente página.
+## Cómo usarla
+Simplemente entra a la página web, busca la clase en la que estás y entra al grupo de WhatsApp correspondiente. Si un grupo de WhatsApp aún no ha sido añadido y eres el administrador de dicho grupo (o tienes el enlace), puedes agregarlo con el botón de la misma web, sólo sigue las instrucciones que se muestran en la página.
 
-# Cómo funciona
-La página web está hecha con [*jekyll*](https://jekyllrb.com) (y usa el tema [*Cayman*](https://github.com/pages-themes/cayman)) esto nos permite generar páginas web con Markdown fácilmente. [*GitHub Pages*](https://pages.github.com/) es usado para hostearla gratis (además que también es compatible con *Jekyll*).
+## Cómo funciona
+La página web está hecha con [*jekyll*](https://jekyllrb.com) (y usa el tema [*Cayman*](https://github.com/pages-themes/cayman)). Esto permite generar páginas web con Markdown fácilmente. Se utiliza [*GitHub Pages*](https://pages.github.com/) para servir la página de manera gratuita (además de que también es compatible con *Jekyll*).
 
-En el caso del sistema para agregar los grupos de WhatsApp automáticamente hago uso de [GitHub Actions](https://github.com/features/actions), los cuales se activan por medio de issues abiertos con el prefijo `bot|`, para esto hacemos un workflow que
+En el caso del sistema para agregar los grupos de WhatsApp, hago uso de [GitHub Actions](https://github.com/features/actions), tareas programadas que se activan al abrir issues con el prefijo `bot|`. Si el issue está en el formato correcto, se cerrará atuomáticamente y el grupo aparecerá en la página en cuestión de minutos. Si el issue no está en el formato solicitado, se mantendrá abierto hasta que sea atendido por mí.
 
-# To do
+## Por hacer
 Con la funcionalidad actual de la página me basta para lo que quería hacer, sin embargo, si hay mucha demanda en alguno de los siguientes elementos *quizás* trabaje en ellos. De igual manera, los [PR](https://github.com/lordfriky/grupos_icom/compare) son **bienvenidos**.
 - [] Agregar un sistema automatizado para generar la página web. (empezado, ver [/tools](https://github.com/lordfriky/grupos_icom/tree/main/tools))
 - [] Agregar [octokit](https://github.com/khornberg/octokit.py) para interactuar mejor con los issues.
--- tools/add_link.py
+++ tools/add_link.py
@@ -1,55 +1,188 @@
-# pip3 install validators
-from validators import url
+# pip3 install bs4
+from datetime import datetime
+import json
+import os
+from re import match
 from sys import argv
-from os.path import exists, join, dirname
+from bs4 import BeautifulSoup
 
-if(len(argv) != 4):
-    print('Uso: python3 add_link.py *clave_de_materia* *nrc* *link_al_grupo*')
-    exit(1)
 
-clave = argv[1]
-nrc = argv[2]
-link = argv[3]
+RAIZ_WEB = os.path.dirname(os.path.dirname(__file__))
+RUTA_MATERIAS = os.path.join(RAIZ_WEB, "materias.json")
 
-if len(clave) != 5:
-    print('Error: La clave {} es inválida'.format(clave))
-    exit(1)
+def obtenerMes(mes: int):
+    meses = [
+        "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio",
+        "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+    ]
+    return meses[mes]
 
-if len(nrc) != 6:
-    print('Error: El NRC {} es inválido'.format(nrc))
-    exit(1)
 
-# Al parecer las instrucciones no fueron muy claras...
-if link.startswith('<'):
-    link = link[1:-1]
+def verificarArgumentos(argumentos: list):
+    if len(argumentos) != 4:
+        print('Uso: python3 add_link.py *clave_de_materia* *nrc* *link_al_grupo*')
+        exit(1)
 
-if not url(link): # To do: Quizás podamos validar que sea un link de WhatsApp, pero igual con esto basta mientras, supongo
-    print('Error: El link {} es inválido'.format(link))
-    exit(1)
+    clave, nrc, link = argv[1:4]
 
-stringAReemplazar = '<a onclick="agregarGrupo(\'{}\', \'{}\')" style="cursor: pointer;"><img src="./res/whatsapp_unavailable.png" width="12"> Agregar</a>'.format(clave, nrc)
-stringReemplazada = '<a href="{}" target="_blank"><img src="./res/whatsapp_available.png" width="12"> Link</a>'.format(link)
+    if len(clave) != 5:
+        print('Error: La clave {} es inválida.'.format(clave))
+        exit(1)
 
-ruta = dirname(__file__)
-ruta = join(ruta, '../index.md')
+    if len(nrc) != 6:
+        print('Error: El NRC {} es inválido.'.format(nrc))
+        exit(1)
 
-if not exists(ruta):
-    print('Error: No se encontró el archivo index.md')
-    exit(1)
+    # Limpiamos por si las instrucciones no fueron claras
+    link = link.strip("<>")
 
-with open(ruta, 'r') as file:
-    datos = file.readlines()
+    # Validamos que sea un link de WhatsApp
+    if match(r"https://(?:www\.)?chat\.whatsapp\.com/[\w]+", link) is None:
+        print('Error: El link {} es inválido. Verifica que el link comience con HTTPS y no HTTP'.format(link))
+        exit(1)
 
-for i in range(len(datos)):
-    if stringAReemplazar in datos[i]:
-        datos[i] = datos[i].replace(stringAReemplazar, stringReemplazada)
-        encontrado = True
-        break
+    return clave, nrc, link
 
-if not encontrado:
-    print('Error: No se pudo encontrar la string en el archivo (Quizás el grupo ya fue añadido?)')
-    exit(1)
 
-with open(ruta, 'w') as file:
-    file.writelines(datos)
+def agregarEnlaceAMaterias(clave: str, nrc: str, link: str):
+    materias = cargarMaterias()
+    if not materias:
+        exit(1)
+
+    try:
+        if materias.get(clave, {}):
+            if materias[clave]["grupos"].get(nrc):
+                materias[clave]["grupos"][nrc]["url"] = link
+            else:
+                print(f"Error: El NRC {nrc} no existe en la base de datos.")
+                exit(1)
+        else:
+            print(f"Error: La clave {clave} no existe en la base de datos.")
+            exit(1)
+
+    except KeyError:
+        import traceback
+        print("Error interno: No se encontró un valor necesario para añadir el grupo.")
+        print("Detalles:")
+        print(traceback.format_exc())
+        exit(1)
+
+    if not guardarMaterias(materias):
+        exit(1)
+
+    return materias
+
+
+def cargarMaterias():
+    materias = {}
+    try:
+        with open(RUTA_MATERIAS, "r") as f:
+            materias = f.read()
+
+        materias = json.loads(materias)
+    except Exception:
+        import traceback
+        print("Error: No se pudieron cargar las materias del JSON")
+        print("Detalles:")
+        print(traceback.format_exc())
+
+    return materias
+
+
+def guardarMaterias(materias: dict):
+    correcto = True
+    try:
+        with open(RUTA_MATERIAS, "w") as f:
+            f.write(json.dumps(materias, indent=4))
+    except Exception:
+        correcto = False
+        import traceback
+        print("Error: No se pudieron guardar las materias en el JSON")
+        print("Detalles:")
+        print(traceback.format_exc())
+
+    return correcto
+
+
+def procesarMateriasAMarkup(materias: dict):
+    document = BeautifulSoup("", "html5lib")
+    document.body.append("---\nlayout: default\n---\n")
+
+    for clave, materia in materias.items():
+        h2 = document.new_tag("h2", style="text-align: center;")
+        h2.string = "{} - {}".format(clave, materia["nombre"])
+
+        document.body.append("\n")
+        document.body.append(h2)
+        document.body.append("\n\n")
+        document.body.append("| NRC | Sección | Maestr@ | Horario | Dias | Edificio | Salón | Grupo de WhatsApp |\n")
+        document.body.append("| --- | ------- | ------- | ------- | ---- | -------- | ----- | ----------------- |\n")
+
+        for grupo in materia["grupos"].values():
+            url = grupo.pop("url")
+            if url:
+                link = document.new_tag("a", href=url, target="_blank")
+                icono = document.new_tag("img", src="./res/whatsapp_available.png", width="18px")
+                link.append(icono)
+                link.append(" Enlace de invitación")
+            else:
+                formatoUrl = "https://github.com/{}/grupos_icom/issues/new?labels=grupo&template=add_group.yml&title=%5BBOT%5D+A%C3%B1adir+enlace+de+invitaci%C3%B3n&clave={}&nrc={}"
+                urlIssue = formatoUrl.format(os.environ.get("GIT_USER"), grupo["clave"], grupo["nrc"])
+                link = document.new_tag("a", href=urlIssue, target="_blank")
+                icono = document.new_tag("img", src="./res/whatsapp_unavailable.png", width="18px")
+                link.append(icono)
+                link.append(" Agregar")
+
+            formato = "| {} | {} | {} | {} | {} | {} | {} | "
+            carSaltoLinea = "  "
+
+            texto = formato.format(grupo["nrc"], grupo["seccion"], grupo["profesor"], carSaltoLinea.join(grupo["horas"]),
+                                   carSaltoLinea.join(grupo["dias"]), carSaltoLinea.join(grupo["edificio"]), carSaltoLinea.join(grupo["aula"]))
+
+            document.body.append(texto)
+            document.body.append(link)
+            document.body.append(" |\n")
+
+    document.body.append("\n")
+
+    piePagina = document.new_tag("p", class_="text-center text-muted")
+    piePagina.string = "Datos actualizados al día "
+
+    ahora = datetime.now()
+    mes = obtenerMes(ahora.month)
+
+    fecha = document.new_tag("b")
+    fecha.string = datetime.strftime(ahora, f"{ahora.day} de {mes} de %Y")
+    piePagina.append(fecha)
+
+    piePagina.append(" a las ")
+
+    hora = document.new_tag("b")
+    hora.string = datetime.strftime(ahora, "%I:%M %p")
+    piePagina.append(hora)
+
+    document.body.append(piePagina)
+
+    page = str(document.body)
+    page = page.replace("<body>", "").replace("</body>", "")
+
+    return page
+
+
+def guardarPagina(pagina: str, archivo: str = 'index.md'):
+    ruta = os.path.join(RAIZ_WEB, archivo)
+
+    with open(ruta, 'w') as file:
+        file.write(pagina)
+
+
+def main(args):
+    clave, nrc, link = verificarArgumentos(args)
+    materias = agregarEnlaceAMaterias(clave, nrc, link)
+    paginaMarkup = procesarMateriasAMarkup(materias)
+    guardarPagina(paginaMarkup)
+
+
+if __name__ == "__main__":
+    main(argv)
     
